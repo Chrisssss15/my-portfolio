c7a74b4d6208903e5f396fc29f1cceced99990f1
Chris Tang <1073412@hr.nl>
2025-03-19 18:26:58 +0100

    ADD Projectpage

---
+++ src/assets/pages/Project.jsx
@@ -0,0 +1,15 @@
+import {Link, Outlet} from 'react-router';
+
+function Project() {
+    return (
+        <div>
+            <header>
+            </header>
+            <main>
+                <h1>Projecten</h1>
+            </main>
+        </div>
+    );
+}
+
+export default Project;
