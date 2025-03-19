3cd5d3e8a156ab7058a8b2e91d7316f438ec112a
Chris Tang <1073412@hr.nl>
2025-03-19 18:26:47 +0100

    ADD contactpage

---
+++ src/assets/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import {Link, Outlet} from 'react-router';
+
+function Contact() {
+    return (
+        <div>
+            <header>
+            </header>
+            <main>
+                <h1>Contact</h1>
+            </main>
+        </div>
+    );
+}
+
+export default Contact;
