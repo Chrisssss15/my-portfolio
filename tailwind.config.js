8f444325c4606accaaa2ea2c9a77c43fd5d229d3
Chris Tang <1073412@hr.nl>
2025-04-07 09:29:29 +0200

    UPDATE tailwind.config.js, add keyframes for animations in the projectDetailPage

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        typing: {
+          '0%': { width: '0ch' },
+          '100%': { width: '35ch' }, // pas aan op lengte van je tekst
+        },
+        blink: {
+          '0%, 100%': { borderColor: 'transparent' },
+          '50%': { borderColor: '#ECA400' },
+        },
+      },
+      animation: {
+        typing: 'typing 3s steps(35, end) forwards',
+        blink: 'blink 0.8s step-end infinite',
+      },
+    },
   },
   plugins: [],
 }
-
