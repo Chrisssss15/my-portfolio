05eaf5c3ef3e772f7ba68065ce98b92c9d3347a0
Chris Tang <1073412@hr.nl>
2025-04-08 17:12:22 +0200

    UPDATE responsive maken

--- src/assets/pages/Studie.jsx
+++ src/assets/pages/Studie.jsx
@@ -85,9 +85,10 @@ import React from "react";
 
 const Studie = () => {
     return (
-        <div className="flex justify-center items-center min-h-screen bg-[#D7D7D7] p-6">
-            <div className="max-w-3xl w-full bg-[#FBF5F3] text-[#000807] p-10 shadow-2xl rounded-lg relative">
-                <div className="absolute top-0 left-1/3 w-1 bg-black h-full"></div>
+        <div className="flex justify-center items-center min-h-screen bg-[#D7D7D7] p-4 sm:p-6">
+            <div className="max-w-3xl w-full bg-[#FBF5F3] text-[#000807] p-6 sm:p-10 shadow-2xl rounded-lg relative">
+                {/* Verticale lijn alleen op desktop */}
+                <div className="hidden md:block absolute top-0 left-[35.5%] w-1 bg-black h-full"></div>
 
                 {[
                     {
@@ -118,14 +119,19 @@ const Studie = () => {
                         ]
                     }
                 ].map((studie, index) => (
-                    <div className="relative flex mb-10" key={index}>
-                        <div className="w-1/3 text-right pr-6">
+                    <div className="relative flex flex-col md:flex-row mb-10" key={index}>
+                        {/* Periode */}
+                        <div className="md:w-1/3 text-left md:text-right pr-0 md:pr-6 mb-2 md:mb-0">
                             <h3 className="text-sm md:text-lg font-semibold">{studie.periode}</h3>
                         </div>
-                        <div className="relative w-8 flex justify-center">
-                            <div className="absolute top-2 w-4 h-4 bg-[#39A56F] border-4 border-[#FBF5F3] rounded-full shadow-md"></div>
+
+                        {/* Tijdlijn bolletje */}
+                        <div className="relative w-8 flex justify-center mx-auto md:mx-0">
+                            <div className="absolute top-2 md:top-3 w-4 h-4 bg-[#39A56F] border-4 border-[#FBF5F3] rounded-full shadow-md hidden md:block"></div>
                         </div>
-                        <div className="w-3/4 pl-6">
+
+                        {/* Inhoud */}
+                        <div className="md:w-2/3 w-full pl-0 md:pl-6">
                             <p className="text-md md:text-lg font-semibold">{studie.school}</p>
                             <ul className="text-sm text-gray-700 list-disc list-inside mt-1 leading-relaxed">
                                 {studie.details.map((item, i) => (
